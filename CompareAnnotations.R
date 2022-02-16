64eb7da93eac03e24a8b991bda6537728f88e004
Mikael Lenz Strube <milst@dtu.dk>
2022-02-16 17:03:00 +0100

    added comparison of annotations

--- CompareAnnotations.R
+++ CompareAnnotations.R
@@ -5,7 +5,7 @@ library(Rsubread)
 safA=flattenGTF(
   GTFfile =  "PROKKA_03102021_correct_locus_tag.gff", 
   GTF.featureType = "CDS",
-  GTF.attrType = "product",
+  GTF.attrType = "locus_tag",#"product",
   # the option specifying the merging algorithm
   method = "merge")
 
@@ -32,5 +32,19 @@ length(which(safB2$GeneID=="hypothetical protein"))
 
 SafA_chr1=subset(safA2, Chr=="1")
 SafB_chr1=subset(safB2, Chr=="CP080275")
+
 both_chr1=merge(x = SafA_chr1 ,y =  SafB_chr1, by.x="Start", by.y="Start", all=T)
+
+both_chr_all=both_chr1
+for(i in 2:5) {
+  SafA_chr=subset(safA2, Chr==unique(safA2$Chr)[i])
+  SafB_chr=subset(safB2, Chr==unique(safB2$Chr)[i])
+  
+  both_chr=merge(x = SafA_chr ,y =  SafB_chr, by.x="Start", by.y="Start", all=T)
+  
+  both_chr_all=rbind(both_chr_all, both_chr )
+}
+
+write.csv(x = both_chr, file = "BothAnnotations.csv", row.names = F)
+
 
